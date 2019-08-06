53837fba529dc657e2077de4bb63e330c51889ba
Zolton <gmzesq@gmail.com>
2019-08-06 16:23:51 -0400

    Basic CRUD works, just need to tailor to MVP specifications

--- commentRouter/commentRouter.js
+++ commentRouter/commentRouter.js
@@ -1,13 +1,15 @@
 const router = require("express").Router()
-
 let data = require("../data/db")
+
+//BASE URL IN THIS ROUTER: 
+//"localhost:8000/api/posts/comments"
 
 router.get("/test", (req, res)=>{
     console.log("hello from commentRoute")
     res.status(200).json({message: "Comment directory found"})
 })
 
-
+//GET all comments from Post ID
 router.get("/:id", (req, res)=>{
     const id = req.params.id
     // ...Run this command
@@ -17,6 +19,16 @@ router.get("/:id", (req, res)=>{
         // map thru it and return it all
         res.status(200).json(posts)
     })
+})
+
+// Had problems - the BODY of the comment has to contain the postID
+//  URL is just base - body handles where it goes
+router.post("/", (req, res)=>{
+    const comment = req.body
+    data.insertComment(comment)
+        .then(newComment=>{
+            res.status(200).json(newComment)
+        })
 })
 
 module.exports = router
--- postRouter/postRouter.js
+++ postRouter/postRouter.js
@@ -8,6 +8,9 @@ router.use("/comments", commentRouterFile)
 //import database
 let data = require("../data/db")
 
+//BASE URL IN THIS ROUTER: 
+//"localhost:8000/api/posts"
+
 //If you receive a GET request at this address...
 router.get("/", (req, res)=>{
     // ...Run this command
@@ -19,8 +22,55 @@ router.get("/", (req, res)=>{
     })
 })
 
+//Get by post ID
+router.get("/:id", (req, res)=>{
+    const id = req.params.id
+    data.findById(id)
+    .then(post=>{
+        console.log(req.body)
+        res.status(200).json(post)
+    })
+})
+
+//POST Posts
+router.post("/", (req, res)=>{
+    const newPost = req.body
+    data.insert(newPost)
+        .then(post=>{
+            res.status(200).json(post)
+        })
+        .catch(error => {
+            res.status(500).json({
+              message: "There was an error while saving the user to the database"
+            });
+          });
+})
+
+//DELETE by ID
+router.delete("/:id", (req, res)=>{
+    const id = req.params.id
+    let spareID = req.params.id
+    // can't grab body.  Copy array first, findPostById, then copy
+    data.remove(id)
+    .then(post=>{
+        console.log(spareID)
+        res.status(200).json({message: `Post deleted: ${spareID}`})
+    })
+
+})
+
 router.get("/test", (req, res)=>{
     res.status(200).json({message: "test directory found"})
+})
+
+// Had problems, didn't realize required 2 arguments
+router.put("/:id", (req, res)=>{
+    const id = req.params.id
+    const updatedPost = req.body
+    data.update(id, updatedPost)
+    .then(update=>{
+        res.status(200).json(update)
+    })
 })
 
 
